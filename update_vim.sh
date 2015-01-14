23fdd7b6a901281e1df757395b8eeb531a8bd940
Zhang Xue <yuki@deep194.siat.ac.cn>
2015-01-14 14:51:57 +0800

    update update_vim.sh

--- update_vim.sh
+++ update_vim.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-if [ ! -d /usr/lib/python2.7/config ]; then
-	echo "directory /usr/lib/python2.7/config not exists"
+config_path="/usr/lib/python2.7/config"
+read -p "where is your config (press y for default $config_path): " key
+if [ $key != "y" ]; then
+	config_path="$key"
+fi
+
+if [ ! -d "$config_path" ]; then
+	echo "directory  $config_path not exists"
 	exit 1
 fi
 
@@ -12,7 +18,7 @@ fi
 cd .tmp
 git clone https://github.com/nschloe/vim.git
 cd vim
-./configure --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config
+./configure --enable-pythoninterp "--with-python-config-dir=$config_path"
 make
 make install
 
